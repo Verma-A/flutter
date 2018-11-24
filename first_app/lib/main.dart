6618641baf4f39c9ce70b3022241cf8ace5425f1
Aman Verma <aman.verma@live.in>
2018-11-25 00:33:49 +0530

    counter starts from 0 now

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -43,7 +43,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 33;
+  int _counter = 0;
 
   void _incrementCounter() {
     setState(() {
