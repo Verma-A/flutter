e4da75acb0d0737204ae2f8dc9a87e67dbd6cebd
verma-a <aman.verma@live.in>
2025-10-23 11:04:02 +0530

    update version

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -3,135 +3,93 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'dart:async';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  final searchKey = TextEditingController();
-  Map data;
-  List<String> datax;
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'New flutter',
-      home: new Scaffold(
-          appBar: AppBar(
-            title: new Text('Image Search'),
-          ),
-          body: FutureBuilder(
-            future: _printText(),
-            builder: (BuildContext context, AsyncSnapshot snap) {
-              if (snap.data == null) {
-                print("snap data is nul====");
-                if (snap.error != null) {
-                  print("===error===: " + snap.error.toString());
-                }
-                return Container(
-                  alignment: Alignment.center,
-                  child: Text('Loading....'),
+      home: const ImageSearchPage(),
+    );
+  }
+}
+
+class ImageSearchPage extends StatefulWidget {
+  const ImageSearchPage({super.key});
+
+  @override
+  State<ImageSearchPage> createState() => _ImageSearchPageState();
+}
+
+class _ImageSearchPageState extends State<ImageSearchPage> {
+  final searchKey = TextEditingController();
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Image Search'),
+      ),
+      body: FutureBuilder(
+        future: _printText(),
+        builder: (BuildContext context, AsyncSnapshot snap) {
+          if (snap.data == null) {
+            if (snap.error != null) {
+              print("===error===: ${snap.error.toString()}");
+            }
+            return const Center(
+              child: Text('Loading....'),
+            );
+          } else {
+            return ListView.builder(
+              itemCount: snap.data.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Card(
+                  child: Image.network(
+                    snap.data[index],
+                    fit: BoxFit.contain,
+                    alignment: Alignment.center,
+                  ),
                 );
-              } else {
-                print("snap data have some data====");
-
-                new ListView.builder(
-                  itemCount: snap.data == null ? 0 : snap.data.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return new Card(
-                        child: new Image(
-                      image: new AssetImage(snap.data[index]),
-                      fit: BoxFit.contain,
-                      alignment: Alignment.center,
-                    ));
-                  },
-                );
-              }
-            },
-          ),
-          // Column(
-          //           children: <Widget>[
-          //             new TextField(
-          //               controller: searchKey,
-          //               decoration: new InputDecoration(
-          //                   labelStyle: new TextStyle(inherit: true),
-          //                   labelText: 'Search',
-          //                   icon: new Icon(Icons.search),
-          //                   border: new OutlineInputBorder(
-          //                       borderRadius: BorderRadius.circular(30.0))),
-          //               onEditingComplete: _printText,
-          //             ),
-          //             Expanded(
-          //                 child: FutureBuilder(
-          //               future: _printText(),
-          //               builder: (BuildContext context, AsyncSnapshot snap) {
-          //                 if (snap.data==null){
-
-          //                   print("snap data is nul====");
-          //                   if (snap.error!=null){
-          //                     print("===error===: "+snap.error.toString());
-          //                   }
-          //                   return Container(
-          //                     alignment: Alignment.center,
-          //                     child: Text('Loading....'),
-          //                   );
-          //                 }else{
-          //                   print("snap data have some data====");
-
-          //                 new ListView.builder(
-          //                   itemCount: snap.data == null ? 0 : snap.data.length,
-          //                   itemBuilder: (BuildContext context, int index) {
-          //                     return new Card(
-          //                         child: new Image(
-          //                       image: new AssetImage(snap.data[index]),
-          //                       fit: BoxFit.contain,
-          //                       alignment: Alignment.center,
-          //                     ));
-          //                   },
-          //                 );
-          //                 }
-          //               },
-          //             ))
-          //           ],
-          //         ),
-          // chlid:new ButtonBar()
-          floatingActionButton: new IconButton(
-            icon: new Icon(Icons.search),
-            onPressed: () {
-              setState:
-              () {
-                _printText();
-              };
-            },
-            splashColor: Colors.blueGrey,
-            color: Colors.blue,
-            highlightColor: Colors.blue,
-            alignment: Alignment.center,
-            // alignment: Alignment.topRight,
-          )
-          // new ButtonTheme(
-          //   textTheme: ButtonTextTheme.normal
-          //   // layoutBehavior: ButtonBarLayoutBehavior.padded,
-          // )
-          ),
+              },
+            );
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            _printText();
+          });
+        },
+        child: const Icon(Icons.search),
+      ),
     );
   }
 
   Future<List<String>> _printText() async {
-    String url;
-    url =
-        "https://www.googleapis.com/customsearch/v1?key=AIzaSyBPlbg3ncXsxjAnVGlA83Vo3hzuTzGA-Fs&cx=015364390810903764394:nvjacwgxlms&imgSize=medium&searchType=image&q=" +
-            searchKey.text;
-    var res = await http.get(url);
-    data = jsonDecode(res.body);
-    String temp;
+    // Mock data - replace with actual API call when needed
     List<String> d = [];
-    print("=========Value of rd=======: " + d.toString());
-    if (data == null) {
-      print("=====data is null");
-    }
-    for (var x in data["items"]) {
-      temp = x["link"];
-      d.add(temp);
-    }
+    d.add('https://picsum.photos/400/300?random=1');
+    d.add('https://picsum.photos/400/300?random=2');
+    d.add('https://picsum.photos/400/300?random=3');
+    d.add('https://picsum.photos/400/300?random=4');
+    d.add('https://picsum.photos/400/300?random=5');
+    
+    // Uncomment below when you have a valid API key:
+    // String url = "https://www.googleapis.com/customsearch/v1?key=YOUR_API_KEY&cx=YOUR_CX&imgSize=medium&searchType=image&q=${searchKey.text}";
+    // var res = await http.get(Uri.parse(url));
+    // Map<String, dynamic> data = jsonDecode(res.body);
+    // List<String> d = [];
+    // if (data["items"] != null) {
+    //   for (var x in data["items"]) {
+    //     d.add(x["link"]);
+    //   }
+    // }
+    
     return d;
   }
 }
